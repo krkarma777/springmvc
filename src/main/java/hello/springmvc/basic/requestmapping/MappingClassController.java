d9e4316e4fdaaf40e821f1afc338d73591d5de28
krkarma777 <krkarma777@gmail.com>
2023-12-13 00:09:12 +0900

    ;

--- src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 public class MappingClassController {
 
     @GetMapping
-    public String user() {
+    public String users() {
         return "get users";
     }
 
@@ -18,16 +18,15 @@ public class MappingClassController {
 
     @GetMapping("/{userId}")
     public String findUser(@PathVariable String userId) {
-        return "get userId=" + userId;
+        return "get userId" + userId;
     }
 
     @PatchMapping("/{userId}")
     public String updateUser(@PathVariable String userId) {
-        return "update userId=" + userId;
+        return "update userId" + userId;
     }
-
     @DeleteMapping("/{userId}")
     public String deleteUser(@PathVariable String userId) {
-        return "delete userId=" + userId;
+        return "delete userId" + userId;
     }
 }
--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -1,4 +1,38 @@
 package hello.springmvc.basic.requestmapping;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
 public class MappingController {
+
+    @RequestMapping(value = {"/hello-basic", "/hello-go"}, method = RequestMethod.GET)
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET)
+    public String mappingGetV1() {
+        log.info("mappingGetV1");
+        return "ok";
+    }
+    @GetMapping(value = "/mapping-get-v2")
+    public String mappingGetV2() {
+        log.info("mappingGetV2");
+        return "ok";
+    }
+
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data) {
+        log.info("mappingPath userId={}", data);
+        return "ok";
+    }
+    @GetMapping("/mapping/{userId}/order/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable Long orderId) {
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
 }
